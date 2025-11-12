63d24a839099009040434f532502f6a33610eac1
Cristian Alvarez <115294338+cristianalvarez1404@users.noreply.github.com>
2025-11-11 21:09:13 -0500

    Refactor SQL queries for clarity and efficiency

--- SQLQuery41.sql
+++ SQLQuery41.sql
@@ -129,6 +129,84 @@ FROM bronze.erp_px_cat_g1v2
 
 
 
+/*===================================================================*/
+
+SELECT cst_id, COUNT(*) FROM
+	(SELECT
+		ci.cst_id,
+		ci.cst_key,
+		ci.cst_firstname,
+		ci.cst_lastname,
+		ci.cst_marital_status,
+		ci.cst_gndr,
+		ci.cst_create_date,
+		ca.bdate,
+		ca.gen,
+		la.cntry
+	FROM silver.crm_cust_info AS ci
+	LEFT JOIN silver.erp_cust_az12 AS ca
+	ON ci.cst_id = ca.cid
+	LEFT JOIN silver.erp_loc_a101 AS la
+	ON ci.cst_ley = la.cid
+)t GROUP BY cst_id
+HAVING COUNT(*) > 1
+
+/*===================================================================*/
+
+CREATE VIEW gold.dim_customers AS 
+SELECT DISTINCT
+	ROW_NUMBER() OVER(ORDER BY cst_id) AS customer_key,
+	ci.cst_id AS customer_id,
+	ci.cst_key AS customer_number,
+	ci.cst_firstname AS first_name,
+	ci.cst_lastname AS last_name,
+	la.cntry AS country
+	ci.cst_marital_status AS marital_status,
+	CASE WHEN ci.cst_gndr != 'n/a' THEN ci.cst_gndr --CRM is the Master for gender info
+		ELSE COALENSCE(ca.gen, 'n/a')
+	END AS gender,
+	ca.bdate AS birthdate,
+	ci.cst_create_date AS create_date
+FROM silver.crm_cust_info AS ci
+LEFT JOIN silver.crm_cust_info AS ca
+ON ci.cst_key = ca.cid
+LEFT JOIN silver.erp_loc_a101 AS la
+ON ci.cst_key = la.cid
+
+
+/*===================================================================*/
+/*SELECT prd_key, COUNT(*) FROM (
+*/
+CREATE VIEW gold.dim_products AS 
+SELECT
+	ROW_NUMBER() OVER(ORDER BY pn.prd_start_dt, pn.prd_key) AS product_key,
+	pn.prd_id AS product_id,
+	pn.prd_key AS product_number,
+	pn.prd_nm AS product_name,
+	pn.cat_id AS category_id,
+	pc.cat AS category,
+	pc.subcat AS subcategory,
+	pc.maintenance,
+	pn.prd_cost AS cost,
+	pn.prd_line AS product_line,
+	pn.prd_start_dt AS start_date,
+FROM silver.crm_prd_info AS pn
+LEFT JOIN silver.erp_px_cat_g1v2 AS pc
+ON pn.cat_id = pc.id
+WHERE prd_end_dt IS NULL -- Filter out all historical data
+
+/*
+)t GROUP BY prd_key
+HAVING COUNT(*) > 1
+*/
+
+
+
+
+
+
+
+
 
 
 
